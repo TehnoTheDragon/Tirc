eadf1ce5ac07fc1da94eeea806926ce98e28b2bb
TehnoTheDragon <tehnothedragon@gmail.com>
2023-05-11 23:16:55 +0300

    updated cmake modules

--- cmake/project_module.cmake
+++ cmake/project_module.cmake
@@ -2,19 +2,32 @@
 include(cmake/file_macros.cmake)
 
 # Module
-function(NewProject ProjectName LanguageName Version)
-    project(${ProjectName} VERSION ${Version} LANGUAGES ${LanguageName})
-endfunction(NewProject)
 
-function(AddSource ProjectName)
-    CollectFiles(_SOURCE ${ARGV})
-    add_executable(${ProjectName} ${_SOURCE})
-endfunction(AddSource)
+function(new_project project language version)
+    project(${project} LANGUAGES ${language} VERSION ${version})
+endfunction(new_project)
 
-macro(IncludeDirs)
-    target_include_directories(${ARGV})
-endmacro(IncludeDirs)
+function(project_add_executable project files)
+    collect_files(out "${files}")
+    add_executable(${project} ${out})
+endfunction(project_add_executable)
 
-function(ProjectProperty ProjectName Property Value)
-    set_property(TARGET ${ProjectName} PROPERTY ${Property} ${Value})
-endfunction(ProjectProperty)
+function(project_add_library project files)
+    collect_files(out "${files}")
+    add_library(${project} ${out})
+endfunction(project_add_library)
+
+function(project_property project property value)
+    set_property(TARGET ${project} PROPERTY ${property} ${value})
+endfunction(project_property)
+
+function(create_ide_filters files)
+    collect_files(out "${files}")
+
+    foreach(file ${out})
+        file(RELATIVE_PATH rel_file ${CMAKE_CURRENT_SOURCE_DIR} ${file})
+        get_filename_component(file_dir ${rel_file} DIRECTORY)
+        string(REPLACE "/" "\\" file_dir ${file_dir})
+        source_group("${file_dir}" FILES ${file})
+    endforeach(file)
+endfunction(create_ide_filters)
