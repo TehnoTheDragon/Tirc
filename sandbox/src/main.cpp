1b4730e2766cfe0e21972212c53a1d3d8d8047d1
TehnoTheDragon <tehnothedragon@gmail.com>
2023-05-13 12:19:31 +0300

    sandboxing the basm parser

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,5 +1,17 @@
-#include <lexer/lexer.hpp>
+#include <iostream>
+#include <basm/parser/basm/basm_parser.hpp>
 
 int main() {
+    try {
+        basm::BasmParser basmParser(R"(
+            @begin: {
+                jump @begin
+            }
+        )");
+        basm::BasmAST* root = basmParser.parse();
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
+
     return 0;
 }
